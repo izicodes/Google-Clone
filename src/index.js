7714fdc2ee2c78c0d4c63a9289a3ccd5b14d50bb
Isabelle <loariyafiaba@gmail.com>
2023-08-13 19:37:03 +0100

    Added the viewing the darkMode button!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const inputField = document.querySelector("#inputField");
 const xMark = document.querySelector("#xMark");
 const searchBtn = document.querySelector("#searchBtn");
+const darkModeSettings = document.querySelector("#darkModeSettings");
+const darkModeBtn = document.querySelector("#darkModeBtn");
+const settingBtn = document.querySelector("#settingBtn");
+
+darkModeSettings.style.display = "none";
+
+settingBtn.addEventListener("click", function () {
+	var status = darkModeSettings.style.display;
+
+	if (status == "none") {
+		darkModeSettings.style.display = "block";
+	} else {
+		darkModeSettings.style.display = "none";
+	}
+});
+
+darkModeBtn.addEventListener("click", function () {
+	var mode = darkModeSettings.dataset.mode;
+
+	if (mode == "Off") {
+		darkModeBtn.innerHTML = 'Dark Mode: On <i class="fa-solid fa-toggle-on"></i>';
+		darkModeSettings.setAttribute("data-mode", "On");
+	} else if (mode == "On") {
+		darkModeBtn.innerHTML = 'Dark Mode: Off <i class="fa-solid fa-toggle-off"></i>';
+		darkModeSettings.setAttribute("data-mode", "Off");
+	}
+});
 
 function showXMark() {
 	var length = inputField.value.length;
